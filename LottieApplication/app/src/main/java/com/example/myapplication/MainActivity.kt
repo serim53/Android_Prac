6cec4eb17adbba2cf2b7902708cfe8aed6690f63
serim53 <tpfla9953@naver.com>
2022-05-29 22:32:26 +0900

    [FEAT] 클릭 이벤트 추가

--- LottieApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ LottieApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.example.myapplication
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mbinding: ActivityMainBinding? = null
+    private val binding get() = mbinding!!
+    private var isClicked: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    fun onClickButton(view: View) {
+        if(!isClicked) {
+            val animator = ValueAnimator.ofFloat(0f, 0.5f).setDuration(500)
+            animator.addUpdateListener {
+                binding.lottieView.progress = it.animatedValue as Float
+            }
+            animator.start()
+            isClicked = true
+        } else {
+            val animator = ValueAnimator.ofFloat(0.5f, 1f).setDuration(500)
+            animator.addUpdateListener {
+                binding.lottieView.progress = it.animatedValue as Float
+            }
+            animator.start()
+            isClicked = false
+        }
     }
 }
